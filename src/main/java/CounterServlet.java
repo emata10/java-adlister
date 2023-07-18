6b72434ad5ae3df785f07bd6c05e4a531a6ad366
emata10 <130502916+emata10@users.noreply.github.com>
2023-07-18 13:47:30 -0600

    servlets-exercise

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+
+    private int count = 0;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        count++;
+        String reset = req.getParameter("reset");
+        if (reset != null) {
+            count = 1;
+        }
+        out.println("Page view count is " + count);
+    }
+}
