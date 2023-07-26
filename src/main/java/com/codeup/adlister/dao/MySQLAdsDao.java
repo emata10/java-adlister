3a956a3bdbc486f6742bdb600217922467afb0ce
emata10 <130502916+emata10@users.noreply.github.com>
2023-07-26 10:45:07 -0600

    fixed config

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -2,11 +2,6 @@ package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.Ad;
 import com.mysql.cj.jdbc.Driver;
-
-import javax.servlet.jsp.jstl.core.Config;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +13,7 @@ public class MySQLAdsDao implements Ads {
         try {
             DriverManager.registerDriver(new Driver());
             connection = DriverManager.getConnection(
-                config.getUrl(),
+               config.getUrl(),
                 config.getUser(),
                 config.getPassword()
             );
